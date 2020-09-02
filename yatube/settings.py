c9f3143e2d18339734036b803e9aae5100bb2ac2
sag163 <60653379+sag163@users.noreply.github.com>
2020-09-03 00:18:40 +0400

    Update settings.py

--- yatube/settings.py
+++ yatube/settings.py
@@ -46,8 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    "debug_toolbar",
     'rest_framework.authtoken',
     'sorl.thumbnail',
 ]
@@ -60,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -155,9 +152,7 @@ CACHES = {
                 'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         }
 }
-INTERNAL_IPS = [
-        "127.0.0.1",
-]
+
 
 REST_FRAMEWORK = {        
     'DEFAULT_PERMISSION_CLASSES': [
